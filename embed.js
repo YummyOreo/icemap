ece3f7db38b406aafe0c69e42372506d9cbaae17
Quinn <quinn@quinnyates.me>
2025-12-09 11:49:22 -0600

    feat: stuff

--- embed.js
+++ embed.js
@@ -4,7 +4,7 @@ let enabledFeatures = loc.searchParams.get("features").split(",")
 let heatmap = loc.searchParams.get("heatmap")
 let latlng = [loc.searchParams.get("lat"), loc.searchParams.get("lng")]
 let y = loc.searchParams.get('y')
-console.log(enabledFeatures)
+let date = loc.searchParams.get('date')
 
 // makeMap([41.848, -87.665], 10, 20)
 makeMap(latlng, y, 20)
@@ -14,20 +14,43 @@ if (enabledFeatures.includes("default")) {
 }
 
 fetch("sightings.json")
-    .then((response) => response.json()) // Parse JSON
-    .then((data) => {
-        let builtData = buildData(data)
-	    if (enabledFeatures.includes("sightings")) {
-		renderFeaturesAll(builtData, "sightings")
-	    }
-	    if (enabledFeatures.includes("protests")) {
-		renderFeaturesAll(builtData, "protests")
-	    }
-	    if (enabledFeatures.includes("helis")) {
-		renderFeaturesAll(builtData, "helis")
-	    }
-	    if (enabledFeatures.includes("other")) {
-		renderFeaturesAll(builtData, "other")
-	    }
-    }) // Work with JSON data
-    .catch((error) => console.error("Error fetching JSON:", error));
+	.then((response) => response.json()) // Parse JSON
+	.then((data) => {
+		let builtData = buildData(data)
+		if (heatmap) {
+			if (date) {
+				renderHeatmapDay(builtData, new Date(date), "sightings")
+			} else {
+				renderHeatmapAll(builtData, "sightings")
+			}
+		}
+		if (enabledFeatures.includes("sightings")) {
+			if (date) {
+				renderFeaturesDay(builtData, new Date(date), "sightings")
+			} else {
+				renderFeaturesAll(builtData, "sightings")
+			}
+		}
+		if (enabledFeatures.includes("protests")) {
+			if (date) {
+				renderFeaturesDay(builtData, new Date(date), "protests")
+			} else {
+				renderFeaturesAll(builtData, "protests")
+			}
+		}
+		if (enabledFeatures.includes("helis")) {
+			if (date) {
+				renderFeaturesDay(builtData, new Date(date), "helis")
+			} else {
+				renderFeaturesAll(builtData, "helis")
+			}
+		}
+		if (enabledFeatures.includes("other")) {
+			if (date) {
+				renderFeaturesDay(builtData, new Date(date), "other")
+			} else {
+				renderFeaturesAll(builtData, "other")
+			}
+		}
+	}) // Work with JSON data
+	.catch((error) => console.error("Error fetching JSON:", error));
